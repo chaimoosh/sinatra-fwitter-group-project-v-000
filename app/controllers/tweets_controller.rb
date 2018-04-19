1b3b1e090703f0ac422f78b65ad57b0939b5072b
Chaim Reisman <crreisman@gmail.com>
2018-04-19 15:17:05 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,6 +31,13 @@ class TweetsController < ApplicationController
    end
  end
 
+ patch '/tweets/:id' do
+  @tweet = Tweet.find_by_id(params[:id])
+  @tweet.update(content: params[:content]})
+  @tweet.save
+  redirect_to "/tweets/#{@tweet.id}"
+end
+
  delete '/tweets/:id/delete' do
     @tweet = Tweet.find_by_id(params[:id])
     @tweet.destroy
