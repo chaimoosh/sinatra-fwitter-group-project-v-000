1a5404f4bcb9ec6fcaec90afcbc6b5c4d0874258
Chaim Reisman <crreisman@gmail.com>
2018-04-19 14:11:21 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(:username => params["username"])
-    binding.pry
+    #binding.pry
     if @user == nil
       redirect to "/signup"
     else
