a28035dde77545a2b8d2b4ceda548f23bc70a433
Chaim Reisman <crreisman@gmail.com>
2018-04-19 15:21:27 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -35,14 +35,14 @@ class TweetsController < ApplicationController
    end
  end
 
- get '/tweets/:id/edit' do 
+ get '/tweets/:id/edit' do
    if logged_in?
      @tweet = Tweet.find_by_id(params[:id])
      erb :'/tweets/edit_tweet'
    else
      redirect to "/login"
    end
- end 
+ end
 
  patch '/tweets/:id' do
   @tweet = Tweet.find_by_id(params[:id])
