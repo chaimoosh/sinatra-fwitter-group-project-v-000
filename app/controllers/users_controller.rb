1298f1682fe415c891531eaba8b84094c711c389
Chaim Reisman <crreisman@gmail.com>
2018-04-19 14:06:03 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(:username => params["username"])
     session[:user_id] = @user.id
+    binding.pry
     redirect to "/tweets"
   end
 
