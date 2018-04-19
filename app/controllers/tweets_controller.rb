7902a9063844ee5a952a2e65ea06ca0cf293467a
Chaim Reisman <crreisman@gmail.com>
2018-04-19 16:08:27 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -45,10 +45,10 @@ class TweetsController < ApplicationController
  end
 
  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
    if params["content"].empty?
-     redirect to "/tweets/new"
+     redirect to "/tweets/#{@tweet.id}/edit"
    else
-     @tweet = Tweet.find_by_id(params[:id])
      @tweet.update(content: params[:content])
      @tweet.save
      redirect to "/tweets/#{@tweet.id}"
