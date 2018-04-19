602ab64d2465a4207f7730a84014411670559227
Chaim Reisman <crreisman@gmail.com>
2018-04-19 14:09:55 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,12 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(:username => params["username"])
     binding.pry
-    session[:user_id] = @user.id
-    redirect to "/tweets"
+    if @user == nil
+      redirect to "/signup"
+    else 
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    end 
   end
 
   get '/logout' do
