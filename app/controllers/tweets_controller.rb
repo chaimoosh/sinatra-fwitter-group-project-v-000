3769594cea72201ed49cc0e24dcc1ccc1b9a9a8b
Chaim Reisman <crreisman@gmail.com>
2018-04-19 15:30:41 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -48,7 +48,7 @@ class TweetsController < ApplicationController
   @tweet = Tweet.find_by_id(params[:id])
   @tweet.update(content: params[:content])
   @tweet.save
-  binding.pry
+  #binding.pry
   redirect_to "/tweets/#{@tweet.id}"
 end
 
